f8018fc43a7f5e073cfb4f1c4ec660363e033df3
karguello90 <karguello90@gmail.com>
2023-04-13 16:51:43 -0400

    updating database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -19,7 +19,21 @@ export const putDb = async (content) => {
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  
+  const jateDB = await openDB('jate', 1);
+
+  // Create a new transaction and specify the database and data privileges.
+  const tx = jateDB.transaction('jate', 'readonly');
+
+  // Open up the desired object store.
+  const store = tx.objectStore('jate');
+
+  // Use the .getAll() method to get all data in the database.
+  const request = store.getAll();
+
+  // Get confirmation of the request.
+  const result = await request;
+  console.log('result.value', result);
+  return result.value;
 }
 
 initdb();

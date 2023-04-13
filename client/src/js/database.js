ee0bfa1a2cc5cd8a8e71855b98611699a4c4ed07
karguello90 <karguello90@gmail.com>
2023-04-13 16:57:11 -0400

    added put function to database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,7 +14,23 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
+  const jateDB = await openDB('jate', 1);
 
+  // Create a new transaction and specify the database and data privileges.
+  const tx = jateDB.transaction('jate', 'readwrite');
+
+  // Open up the desired object store.
+  const store = tx.objectStore('jate');
+
+  const request = store.put({
+    id: 1,
+    value: content
+  });
+
+  // Get confirmation of the request.
+  const result = await request;
+  console.log('result.value', result);
+  //return result.value;
 }
 
 // TODO: Add logic for a method that gets all the content from the database
